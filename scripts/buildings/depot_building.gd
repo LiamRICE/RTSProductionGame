c98efa4bde45e6996f411aa159853f2d283ab367
LiamLucRice <liam@eoi-b4purpose.com>
2025-03-26 16:56:58 +0700

    Updated depot building navigation nodes

--- scripts/buildings/depot_building.gd
+++ scripts/buildings/depot_building.gd
@@ -2,5 +2,6 @@ class_name DepotBuilding extends Building
 
 const resource_utils:GDScript = preload("res://scripts/utilities/resource_utils.gd")
 
+
 func drop_off(amount:int, type:resource_utils.Res):
 	pass
