e97c36eb1055342910dbdb383cb1b5cdbc4ab3f6
LiamLucRice <liam@eoi-b4purpose.com>
2025-03-30 12:03:11 +0700

    Fixed UI issues with unit creation

--- scripts/managers/ui_manager.gd
+++ scripts/managers/ui_manager.gd
@@ -14,7 +14,7 @@ var is_on_ui:bool = false:
 
 
 func _ready() -> void:
-	self.player_interface.selection_changed.connect(self.player_interface.orders_interface._on_selection_changed)
+	self.player_interface.selection_changed.connect(self.player_interface.orders_interface._on_player_interface_selection_changed)
 	self.player_interface.mouse_entered.connect(self._on_mouse_entered)
 	self.player_interface.mouse_exited.connect(self._on_mouse_exited)
 
--- scripts/ui/level_ui/bottom_bar_container.gd
+++ scripts/ui/level_ui/bottom_bar_container.gd
@@ -1,5 +1,8 @@
 extends TabContainer
 
+# Loading script classes
+const UIStateUtils := preload("res://scripts/utilities/ui_state_utils.gd")
+
 ## Info bar properties
 @export var abilities_build_container_size:Vector2i = Vector2i(5, 3)
 @export var abilities_build_container:GridContainer
@@ -20,27 +23,28 @@ func _ready() -> void:
 		self.button_list.append(button)
 		self.abilities_build_container.add_child(button)
 
-## Executed when the selection changes
-func _on_selection_changed(new_selection:Array[Entity], entity_type:int) -> void:
-	print("Selection changed")
-	if new_selection.size() == 0:
-		for button in button_list:
-			button.set_disabled(true)
-			button.set_button_icon(null)
-		return
-	## Get the selection's first item
-	var item:Entity = new_selection[0]
-	print("Button icon assigned")
-	if entity_type == 2 and item is ProductionBuilding:
-		for index in range(item.building_units.size()):
-			var unit:Unit = item.building_units[index].entity_instance.instantiate()
-			self.button_list[index].icon = unit.icon
-			self.button_list[index].set_disabled(false)
-			unit.free()
-	self.selection_list = new_selection
 
 ## Executed when a button is pressed
 func _on_button_pressed(index:int) -> void:
 	print("Pressed : ", index)
 	## TODO - execute button's effect on object
 	selection_list[0].queue_unit(index)
+
+
+func _on_player_interface_selection_changed(selection: Array[Entity], selection_type: UIStateUtils.SelectionType) -> void:
+	print("Selection changed")
+	if selection.size() == 0:
+		for button in button_list:
+			button.set_disabled(true)
+			button.set_button_icon(null)
+		return
+	## Get the selection's first item
+	var item:Entity = selection[0]
+	print("Button icon assigned")
+	if selection_type == UIStateUtils.SelectionType.BUILDINGS and item is ProductionBuilding:
+		for index in range(item.building_units.size()):
+			var unit:Unit = item.building_units[index].entity_instance.instantiate()
+			self.button_list[index].icon = unit.icon
+			self.button_list[index].set_disabled(false)
+			unit.free()
+	self.selection_list = selection
